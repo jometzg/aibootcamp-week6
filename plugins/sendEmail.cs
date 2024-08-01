88b3a2d58133471762d63f5e4855ff78f4c9f3ac
John Metzger <56820659+jometzg@users.noreply.github.com>
2024-08-01 15:36:03 +0100

    Create sendEmail.cs

---
+++ plugins/sendEmail.cs
@@ -0,0 +1,31 @@
+#pragma warning disable SKEXP0050
+
+using System.ComponentModel;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Plugins.Web;
+using Microsoft.SemanticKernel.Plugins.Web.Bing;
+
+/// <summary>
+/// A plugin that searches the web to get information that contains the next match date and time for the a passed football team.
+/// https://learn.microsoft.com/en-us/semantic-kernel/concepts/plugins/using-data-retrieval-functions-for-rag
+/// </summary>
+public class SendEmailPlugin //JM+
+{
+    [KernelFunction("web_send_email")]
+    [Description("send the booking confirmation to the customer's email address.")]
+    [return: Description("The body of the email.")]
+    public async Task<string> WebSendEmail([Description("The email address")] string email_address,
+                                        [Description("The booking details")] BookingModel booking_details)
+    {
+        // CHALLENGE 2.4
+        // Write a native function that sends an email to the customer with the booking details.
+        var kernel = Kernel.CreateBuilder().Build();
+        
+        var bingConnector = new BingConnector("<bing-azure-service-key>");
+        kernel.ImportPluginFromObject(new WebSearchEnginePlugin(bingConnector), "bing");
+
+        var function = kernel.Plugins["bing"]["search"];
+        var bingResult = await kernel.InvokeAsync(function, new() { ["query"] = "Send email to "+ email_address + " with the booking details: " + booking_details });
+        return bingResult.ToString();
+    }
+}
